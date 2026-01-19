d488cbb388676d31c17f8747f802ddf82e6c2659
Biswajeet <biswajeet162@gmail.com>
2026-01-19 23:05:18 +0530

    superb all set release model apk also working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            // Disable code shrinking to prevent network code from being stripped
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
